a5869e41dde9da1e19d5c2a3bcc90a7967a90d3e
LucasDev <lucasmndes.dev@gmail.com>
2026-01-31 13:36:22 -0300

    implement the create subject page

--- src/pages/subjects/create.tsx
+++ src/pages/subjects/create.tsx
@@ -1,3 +1,242 @@
-export default function SubjectsCreate(){
-    return <div className="bg-green-500">Create Subject</div>;
-}
+import { useForm } from "@refinedev/react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { CreateView } from "@/components/refine-ui/views/create-view";
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+
+import { Textarea } from "@/components/ui/textarea";
+import { useBack, useList } from "@refinedev/core";
+import { Loader2 } from "lucide-react";
+import { Department } from "@/types";
+import z from "zod";
+
+
+const subjectCreateSchema = z.object({
+    departmentId: z.coerce
+        .number({
+            required_error: "Department is required",
+            invalid_type_error: "Department is required",
+        })
+        .min(
+            1, "Department is required"
+        ),
+    name: z.string().min(3, "Subject name must be at least 3 characters long"),
+    code: z.string().min(3, "Subject code must be at least 3 characters long"),
+    description: z.string().min(5, "Description must be at least 5 characters long")
+})
+
+type SubjectFormValues = z.infer<typeof subjectCreateSchema>;
+
+const SubjectsCreate = () => {
+  const back = useBack();
+
+  const form = useForm({
+    resolver: zodResolver(subjectCreateSchema),
+    refineCoreProps: {
+      resource: "subjects",
+      action: "create",
+    },
+    defaultValues: {
+      departmentId: undefined,
+      name: "",
+      code: "",
+      description: "",
+    },
+  });
+
+  const {
+    refineCore: { onFinish }, //isso é usado para submeter os dados
+    handleSubmit,
+    formState: { isSubmitting },
+    control,
+  } = form;
+
+  const onSubmit = async (values: SubjectFormValues) => {
+    try {
+      await onFinish(values);
+    } catch (error) {
+      console.error("Error creating class:", error);
+    }
+  };
+
+  const { query: departmentsQuery } = useList<Department>({
+    resource: "departments",
+    pagination: {
+      pageSize: 100,
+    }
+  })
+
+  const departments = departmentsQuery.data?.data || [];
+  const departmentsLoading = departmentsQuery.isLoading;
+
+  return (
+    <CreateView className="class-view">
+      <Breadcrumb />
+
+      <h1 className="page-title">Create a Subject </h1>
+      <div className="intro-row">
+        <p>Provide the required information below to add a subject.</p>
+        <Button onClick={() => back()}>Go Back</Button>
+      </div>
+
+      <Separator />
+
+      <div className="my-4 flex items-center">
+        <Card className="class-form-card">
+          <CardHeader className="relative z-10">
+            <CardTitle className="text-2xl pb-0 font-bold text-gradient-orange">
+              Fill out form
+            </CardTitle>
+          </CardHeader>
+
+          <Separator />
+
+          <CardContent className="mt-7">
+            <Form {...form}>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <div>
+                  <FormField
+                    control={control}
+                    name="departmentId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Department <span className="text-orange-600">*</span>
+                        </FormLabel>
+                        <Select
+                          onValueChange={(value) =>
+                            field.onChange(Number(value))
+                          }
+                          value={field.value?.toString()}
+                          disabled={departmentsLoading}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="Select a department" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {departments.map((department: Department) => (
+                              <SelectItem
+                                key={department.id}
+                                value={department.id.toString()}
+                              >
+                                {department.name}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                  <FormField
+                    control={control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Subject Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="text"
+                            placeholder="Mathematics"
+                            onChange={(e) => {
+                              const value = e.target.value;
+                              field.onChange(value);
+                            }}
+                            value={field.value ?? ""}
+                            name={field.name}
+                            ref={field.ref}
+                            onBlur={field.onBlur}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={control}
+                    name="code"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Subject Code <span className="text-orange-600">*</span>
+                        </FormLabel>
+                        <Input
+                            type="text"
+                            placeholder="MATH101"
+                            onChange={(e) => {
+                              const value = e.target.value;
+                              field.onChange(value);
+                            }}
+                            value={field.value ?? ""}
+                            name={field.name}
+                            ref={field.ref}
+                            onBlur={field.onBlur}
+                          />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                <FormField
+                  control={control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Brief description about the subject"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Separator />
+
+                <Button type="submit" size="lg" className="w-full">
+                  {isSubmitting ? (
+                    <div className="flex gap-1">
+                      <span>Creating Subject...</span>
+                      <Loader2 className="inline-block ml-2 animate-spin" />
+                    </div>
+                  ) : (
+                    "Create Subject"
+                  )}
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </CreateView>
+  );
+};
+
+export default SubjectsCreate;
